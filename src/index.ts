68c45568bf9bf99177aedf4ddd221e487e7431cc
teplostanski <teplostanski@yandex.ru>
2023-10-15 15:30:03 +0300

    feat: enhance replace utility with regex support and dynamic versioning

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,32 @@
 
 import { Command } from 'commander'
 import { replaceInFile } from './module'
+import pkg from '../package.json' // Путь может отличаться в зависимости от структуры вашего проекта
 
+/**
+ * Command Line Interface (CLI) utility for searching and replacing text in files.
+ * Provides options to specify the file, search pattern, replacement text, and whether the replacement should be global.
+ *
+ * @example
+ * ```bash
+ * $ nreplacer --file ./sample.txt --search Hello --replace Hi
+ * ```
+ *
+ * @example
+ * ```bash
+ * $ nreplacer --file ./sample.txt --search /Hello/g --replace Hi
+ * ```
+ */
 const program = new Command()
 
 program
-  .version('1.0.0')
-  .description('Search and replace text in files')
+  .version(pkg.version) // Используйте версию из package.json
+  .description(pkg.description) // Используйте описание из package.json
   .option('-f, --file <path>', 'Path to the file')
   .option('-s, --search <text>', 'Text to search for')
   .option('-r, --replace <text>', 'Text to replace with')
   .option('-g, --global', 'Replace all occurrences', false)
-  .action((options) => {
+  .action(async (options) => {
     const { file, search, replace, global: globalReplace } = options
 
     if (!file || !search || !replace) {
@@ -21,7 +36,7 @@ program
     }
 
     try {
-      replaceInFile(file, search, replace, globalReplace)
+      await replaceInFile(file, search, replace, globalReplace)
       console.log(`Replaced "${search}" with "${replace}" in ${file}`)
     } catch (error) {
       console.error((error as Error).message)
@@ -30,4 +45,3 @@ program
   })
 
 program.parse(process.argv)
-export { replaceInFile }
--- src/module.ts
+++ src/module.ts
@@ -5,12 +5,40 @@ import { createReadStream, createWriteStream } from 'fs'
 import { Transform } from 'stream'
 import fileType from 'file-type'
 
-export async function replaceInFile(filePath: string, searchValue: string, replaceValue: string, globalReplace = false): Promise<void> {
+/**
+ * Replaces occurrences of a search string or regular expression with a given replacement string in a file.
+ *
+ * @param filePath - The path to the target file.
+ * @param searchValue - The string or regular expression to search for.
+ * @param replaceValue - The string to replace matched values with.
+ * @param globalReplace - If true and searchValue is a string, replace all occurrences. Default is false.
+ *
+ * @throws {Error} - Throws an error if the file does not exist or is not a text file.
+ *
+ * @example
+ * ```typescript
+ * import { replaceInFile } from './replaceModule';
+ *
+ * (async () => {
+ *   await replaceInFile('./sample.txt', 'Hello', 'Hi');
+ * })();
+ * ```
+ *
+ * @example
+ * ```typescript
+ * import { replaceInFile } from './replaceModule';
+ *
+ * (async () => {
+ *   await replaceInFile('./sample.txt', /Hello/g, 'Hi');
+ * })();
+ * ```
+ */
+export async function replaceInFile(filePath: string, searchValue: string | RegExp, replaceValue: string, globalReplace = false): Promise<void> {
   if (!fs.existsSync(filePath)) {
     throw new Error(`File not found: ${filePath}`)
   }
 
-  const type = await fileType.fromFile(filePath) // Используйте асинхронный метод
+  const type = await fileType.fromFile(filePath)
 
   if (type && !type.mime.startsWith('text/')) {
     throw new Error(`Unsupported file type: ${type.mime}. This tool is intended for text files.`)
@@ -22,8 +50,10 @@ export async function replaceInFile(filePath: string, searchValue: string, repla
   const replaceStream = new Transform({
     transform(chunk, _, callback) {
       let data = chunk.toString()
-      const regex = globalReplace ? new RegExp(searchValue, 'g') : new RegExp(searchValue)
-      data = data.replace(regex, replaceValue)
+      if (typeof searchValue === 'string' && globalReplace) {
+        searchValue = new RegExp(searchValue, 'g')
+      }
+      data = data.replace(searchValue, replaceValue)
       this.push(data)
       callback()
     },
