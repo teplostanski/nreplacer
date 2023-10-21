944a4f53d54fb44b28d3ed54d193e8d2b5bbcd0f
teplostanski <teplostanski@yandex.ru>
2023-10-21 23:35:18 +0300

    last commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,13 +2,32 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [0.3.6](https://github.com/teplostanski/nreplacer/compare/v0.3.5...v0.3.6) (2023-10-21)
+
+### [0.3.5](https://github.com/teplostanski/nreplacer/compare/v0.3.4...v0.3.5) (2023-10-16)
+
+### [0.3.4](https://github.com/teplostanski/nreplacer/compare/v0.3.3...v0.3.4) (2023-10-16)
+
+### [0.3.3](https://github.com/teplostanski/nreplacer/compare/v0.3.2...v0.3.3) (2023-10-16)
+
+
+### Features
+
+* **cli:** enhance output formatting and add microsecond precision ([93c9df3](https://github.com/teplostanski/nreplacer/commit/93c9df3f2a1748fa6ea450ffe4f218cd2fb1ef7c))
+
+### [0.3.2](https://github.com/teplostanski/nreplacer/compare/v0.3.1...v0.3.2) (2023-10-16)
+
+
+### Features
+
+* **cli:** enhance CLI output with bordered box and noprint option ([d26b810](https://github.com/teplostanski/nreplacer/commit/d26b810dc159473c32a82f9008341bf33f7f837e))
+
 ### 0.3.1 (2023-10-16)
 
 
 ### Features
 
 * add the main functional ([73d573d](https://github.com/teplostanski/nreplacer/commit/73d573d7ee8570ed2e72c3e1e435354aeceda908))
-* **cli:** Add easter egg with HTTP 418 'Im a teapot' reference ([97db444](https://github.com/teplostanski/nreplacer/commit/97db44401a8d27688d31545ad6a24d3b10f20a12))
 * **cli:** add shorthand flag for version ([8000b5b](https://github.com/teplostanski/nreplacer/commit/8000b5b620e852b2c8cf73792164f7a7edba2b19))
 * **cli:** change line replacement count to search match count ([8bacee3](https://github.com/teplostanski/nreplacer/commit/8bacee360f53eea7e32653fb288fb233b3b57d74))
 * **cli:** Integrate chalk, ora, and verbose options ([c979a1c](https://github.com/teplostanski/nreplacer/commit/c979a1c04ccfbbde1dd16c9ed2690d061b072f64))
--- src/cli.ts
+++ src/cli.ts
@@ -2,10 +2,9 @@
 // cli.ts
 
 import { Command } from 'commander'
-import { replaceInFiles } from './module.js' // Обновленный импорт
+import { replaceInFiles } from './module.js'
 import chalk from 'chalk'
-import ora from 'ora'
-import cliui from 'cliui'
+import boxen from 'boxen'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -17,54 +16,66 @@ const program = new Command()
 program
   .version(version, '-v, --version')
   .description('CLI tool for searching and replacing text within files or directories.')
+  .option('--noprint', 'Disable output printing')
   .option('--nocolor', 'Disable color output')
-  .option('--noverbose', 'Disable verbose output')
   .option('-f, --file <path>', 'Path to the file or directory')
   .option('-s, --search <text>', 'Text or regex to search for')
   .option('-r, --replace <text>', 'Text to replace with')
   .option('-g, --global', 'Replace all occurrences', false)
   .action(async (options) => {
-    const { file: filePathOrDir, search, replace, global: globalReplace, nocolor, noverbose } = options
+    const { file: filePathOrDir, search, replace, global: globalReplace, noprint, nocolor } = options
 
     const log = {
-           error: nocolor ? console.error : (msg: string) => console.error(chalk.red(msg)),
+      error: nocolor ? console.error : (msg: string) => console.error(chalk.red(msg)),
       success: nocolor ? console.log : (msg: string) => console.log(chalk.green(msg)),
       info: nocolor ? console.log : (msg: string) => console.log(chalk.blue(msg)),
     }
 
     if (!filePathOrDir || !search || !replace) {
-      log.error('Please provide required options: --file, --search, --replace')
+      if (!noprint) {
+        log.error('Please provide the required options: --file, --search, --replace')
+      }
       process.exit(1)
     }
 
-    const spinner = ora({
-      text: 'Initializing...',
-      prefixText: '\n',
-      color: nocolor ? 'white' : 'yellow',
-    })
-
-    spinner.start()
-
     try {
-      const startTime = Date.now()
+      const hrStartTime = process.hrtime()
       const results = await replaceInFiles(filePathOrDir, search, replace, globalReplace)
-      const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2)
+      const hrElapsedTime = process.hrtime(hrStartTime)
+      const seconds = hrElapsedTime[0]
+      const milliseconds = Math.floor(hrElapsedTime[1] / 1000000)
+      const microseconds = Math.floor((hrElapsedTime[1] / 1000) % 1000)
 
-      spinner.stop() // Останавливаем прогресс перед выводом информации
+      if (!noprint) {
+        let totalReplacements = 0
 
-      for (const result of results) {
-        log.success(`Replaced "${chalk.yellow(search)}" with "${chalk.yellow(replace)}" in ${chalk.yellow(result.filePath)}`)
-        log.info(`Matches replaced: ${chalk.yellow(result.replacedLines)}`)
-      }
+        for (const result of results) {
+          totalReplacements += result.replacedLines
+        }
 
-      const ui = cliui({ width: 80 })
-      ui.div(chalk.blueBright('Time taken: '), chalk.yellowBright(`${elapsedTime}s`))
-
-      if (!noverbose) {
-        log.info(ui.toString())
+        if (totalReplacements > 0) {
+          let output = `Replaced "${chalk.yellow(search)}" with "${chalk.yellow(replace)}" in:\n`
+          for (const result of results) {
+            if (result.replacedLines > 0) {
+              output += `${chalk.yellow(result.filePath)} | Matches replaced: ${chalk.yellow(result.replacedLines)}\n`
+            }
+          }
+          output += `\n${chalk.blueBright('Time taken:')} ${chalk.yellowBright(`${seconds}s ${milliseconds}.${microseconds}ms`)}`
+          log.info(
+            boxen(output, {
+              padding: 0.5,
+              //borderColor: 'yellow',
+              //margin: 1,
+            }),
+          )
+        } else {
+          log.info(`${chalk.yellow('Warn:')} No matches were replaced in any file.`)
+        }
       }
     } catch (error) {
-      spinner.fail((error as Error).message)
+      if (!noprint) {
+        log.error((error as Error).message)
+      }
       process.exit(1)
     }
   })
